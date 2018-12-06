c233764e8f8c6583ec1ae14d49cca0a1a812f642
lalitsingh <lalitbisalpur@gmail.com>
2018-12-06 12:06:55 +0530

    inital

---
+++ app/src/main/java/fusionsoftware/loop/srimart/activity/SplashActivity.java
@@ -0,0 +1,24 @@
+package fusionsoftware.loop.srimart.activity;
+
+import android.content.Intent;
+import android.os.Handler;
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+
+import fusionsoftware.loop.srimart.R;
+
+public class SplashActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_splash);
+        new Handler().postDelayed(new Runnable() {
+            @Override
+            public void run() {
+                startActivity(new Intent(SplashActivity.this, Dashboardctivity.class));
+                finish();
+            }
+        }, 3000);
+    }
+}
